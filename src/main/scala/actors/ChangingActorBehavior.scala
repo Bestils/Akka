a2031a635926199c7586c424cd03886a7f5955fe
bestils <thebestilspl@gmail.com>
2019-09-12 08:39:01 +0200

    continue first  phate

---
+++ src/main/scala/actors/ChangingActorBehavior.scala
@@ -0,0 +1,24 @@
+package actors
+
+import akka.actor.Actor
+
+object ChangingActorBehavior extends App{
+
+  object FussyKid{
+    case object KidAccept
+    case object KidReject
+  }
+
+  class FussyKid extends Actor {
+    override def receive: Receive = {}
+  }
+
+    object  Mom {
+      case class Food(food: String)
+      case class Ask(message: String)
+    }
+    class Mom extends Actor{
+      override def receive: Receive = ???
+    }
+
+}
