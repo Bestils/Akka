f46c22fef92ad503b7374665ad603e65f45f7136
bestils <thebestilspl@gmail.com>
2019-09-12 13:47:37 +0200

    continue first  phate

--- src/main/scala/actors/ChangingActorBehavior.scala
+++ src/main/scala/actors/ChangingActorBehavior.scala
@@ -1,70 +1,86 @@
 package actors
 
 import actors.ActorCapabilities.{Bank, system}
+import actors.ChangingActorBehavior.Counter.{Decrement, Increment, Print, start}
 import actors.ChangingActorBehavior.FussyKid.{KidAccept, KidReject}
 import actors.ChangingActorBehavior.Mom.MomStart
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorContext, ActorRef, ActorSystem, Props}
 
-object ChangingActorBehavior extends App{
+object ChangingActorBehavior extends App {
 
-  object FussyKid{
+  object FussyKid {
+
     case object KidAccept
+
     case object KidReject
-    val HAPPY ="happy"
+
+    val HAPPY = "happy"
     val SAD = "sad"
   }
 
   class FussyKid extends Actor {
+
     import FussyKid._
     import Mom._
 
     var state = HAPPY
+
     override def receive: Receive = {
       case Food(VEGETABLE) => state = SAD
       case Food(CHOCOLATE) => state = HAPPY
       case Ask(message) =>
-        if(state ==HAPPY) sender() ! KidAccept
+        if (state == HAPPY) sender() ! KidAccept
         else sender() ! KidReject
     }
   }
 
-    object  Mom {
-      case class MomStart(kidRef:ActorRef)
-      case class Food(food: String)
-      case class Ask(message: String)
-    val VEGETABLE = "vegetable"
-      val CHOCOLATE= "chocolate"
+  object Mom {
 
+    case class MomStart(kidRef: ActorRef)
+
+    case class Food(food: String)
+
+    case class Ask(message: String)
+
+    val VEGETABLE = "vegetable"
+    val CHOCOLATE = "chocolate"
+
+  }
+
+  class Mom extends Actor {
+
+    import Mom._
+
+    override def receive: Receive = {
+      case MomStart(kidRef) =>
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Ask("do you want to play?")
+      case KidAccept => println("yay my kid is happy")
+      case KidReject => println("my kid life but he is sad")
     }
-    class Mom extends Actor{
-import Mom._
-      override def receive: Receive = {
-        case MomStart(kidRef) =>
-          kidRef ! Food(VEGETABLE)
-          kidRef ! Ask("do you want to play?")
-        case KidAccept => println("yay my kid is happy")
-        case KidReject => println("my kid life but he is sad")
-      }
-    }
-  class StatelessFussyKid extends  Actor{
-    import  FussyKid._
+  }
+
+  class StatelessFussyKid extends Actor {
+
+    import FussyKid._
     import Mom._
 
     override def receive: Receive = ???
 
     def happyRecieve: Receive = {
-      case Food(VEGETABLE) => context.become(sadRecieve,false)
+      case Food(VEGETABLE) => context.become(sadRecieve, false)
       case Food(CHOCOLATE) =>
-      case Ask(_) => sender() ! KidAccept}
+      case Ask(_) => sender() ! KidAccept
+    }
 
     def sadRecieve: Receive = {
-      case Food(VEGETABLE) =>context.unbecome()
-      case Food(CHOCOLATE) =>context.become(happyRecieve,false  )
+      case Food(VEGETABLE) => context.unbecome()
+      case Food(CHOCOLATE) => context.become(happyRecieve, false)
       case Ask(_) => sender() ! KidReject
     }
   }
 
-val system =ActorSystem("changingActorBehaviorDemo")
+  val system = ActorSystem("changingActorBehaviorDemo")
   val fussyKid = system.actorOf(Props[FussyKid])
   val statelessFussyKid = system.actorOf(Props[StatelessFussyKid])
   val mom = system.actorOf(Props[Mom])
@@ -74,15 +90,17 @@ val system =ActorSystem("changingActorBehaviorDemo")
 
   /**
    * Exercisies
-   *  1- recreate the Counter Actor with context.become and no MUTABLE STATE
+   * 1- recreate the Counter Actor with context.become and no MUTABLE STATE
    */
   object Counter {
 
-    case object Increment
+    case class Increment(number: Int = 1)
 
-    case object Decrement
+    case class Decrement(number: Int = 1)
 
     case object Print
+
+    case class start(number: Int)
 
   }
 
@@ -91,32 +109,75 @@ val system =ActorSystem("changingActorBehaviorDemo")
     import Counter._
     import Bank._
 
-    var count = 0
+
+    def math(number: Int): Receive = {
+      case Increment => context.become(math(number + 1))
+      case Decrement =>
+      case Increment => context.become(math(number - 1))
+      case Print => println(s"SImple actor I have recieverd a NUMBER: $number")
+    }
 
     override def receive: Receive = {
-      case Increment => context.sender() ! count + 1
-      case Decrement => context.sender() ! count - 1
-      case Print => println(s"SImple actor I have recieverd a NUMBER: $count")
+      case start(number: Int) => context.become(math(number))
     }
   }
 
   val counter = system.actorOf(Props[Counter], "myCounter")
+  counter ! start(5)
+  counter ! Increment
+  counter ! Decrement(2)
+  counter ! Increment(7)
+  counter ! Print
 
   /**
    * Exercises 2 - a simplified voting system
    */
 
   case class Vote(candidate: String)
+
   case object VoteStatusRequest
+
   case class VoteStatusReply(candidate: Option[String])
-  class Citizen extends Actor{
-    override def receive: Receive = ???
+
+  var candidate: Option[String] = None
+
+  class Citizen extends Actor {
+    override def receive: Receive = {
+      case Vote(c) => context.become(voted(c))
+      case VoteStatusRequest => sender() ! VoteStatusReply(candidate)
+    }
+    def voted(candidate: String) : Receive = {
+      case VoteStatusRequest => sender() ! VoteStatusReply(Some(candidate))
+    }
+
   }
-  case class AgregateVotes(citizens:Set[ActorRef])
+
+  case class AggregateVotes(citizens: Set[ActorRef])
 
   class VoteAgreggator extends Actor {
-    override def receive: Receive = ???
+
+    override def receive: Receive = awaitingCommand
+
+
+
+    def awaitingCommand : Receive ={
+      case AggregateVotes(citizens) =>
+        citizens.foreach(citizensRef => citizensRef ! VoteStatusRequest)
+        context.become(awaitingStatuses(citizens,Map()))
+    }
+    def awaitingStatuses(stillWaiting: Set[ActorRef],currentStats:Map[String,Int]): Receive ={
+      case VoteStatusReply(Some(candidate)) =>
+        val newStillWaiting = stillWaiting - sender()
+        val currentVotesOfCandidate = currentStats.getOrElse(candidate,0)
+        val newStats = currentStats +(candidate -> (currentVotesOfCandidate+1))
+        if(newStillWaiting.isEmpty) {
+          println(s"$self poll stats: $currentStats")
+        }
+       context.become(awaitingStatuses(newStillWaiting,newStats))
+
+    }
   }
+
   val alice = system.actorOf(Props[Citizen])
   val bob = system.actorOf(Props[Citizen])
   val charlie = system.actorOf(Props[Citizen])
@@ -128,5 +189,13 @@ val system =ActorSystem("changingActorBehaviorDemo")
   daniel ! Vote("Martin")
 
   val voteAgreggator = system.actorOf(Props[VoteAgreggator])
-  voteAgreggator !AgregateVotes(Set(alice,bob,charlie,daniel))
+  voteAgreggator ! AggregateVotes(Set(alice, bob, charlie, daniel))
+
+  /*
+  print the status of the votes
+  Martin -> 1
+  Jonas ->1
+  Roland ->2
+
+   */
 }
